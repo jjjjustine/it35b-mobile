eae91b42bf35631fd15887d34f643154559f5dac
jjjjustine <catingan.janejustine>
2026-03-26 17:54:41 +0800

    added

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { IonCheckbox } from '@ionic/react';
+
+function Example() {
+  return <IonCheckbox>I agree to the terms and conditions</IonCheckbox>;
+}
+export default Example;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCheckbox, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Yoww!!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -17,6 +17,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+      <IonCheckbox />
       </IonContent>
     </IonPage>
   );
