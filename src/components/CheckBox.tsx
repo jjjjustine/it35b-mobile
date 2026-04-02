e13fd44084ecc9d06753895d0b7c2e36405d702f
jjjjustine <catingan.janejustine@gmail.com>
2026-04-02 22:38:04 +0800

    removed file chckbox

--- src/components/CheckBox.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { IonCheckbox } from '@ionic/react';
-
-function Example() {
-  return <IonCheckbox>I agree to the terms and conditions</IonCheckbox>;
-}
-export default Example;
