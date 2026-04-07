6eb6318ef7ef2689e4f389fd581c0a179c229025
jjjjustine <catingan.janejustine@gmail.com>
2026-04-07 08:30:59 +0800

    added changes on home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Yoww!!</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <IonPage>
+          <IonHeader>
+              <IonToolbar>
+                <IonButtons  slot='start'>
+                  <IonMenuButton></IonMenuButton>
+                </IonButtons>
+                  <IonTitle>Yoww!!</IonTitle>
+                          </IonToolbar>
+                  </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Yoww!!</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
