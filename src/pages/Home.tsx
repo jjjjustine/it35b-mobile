ec47f5b52d08717d1adab0b9d892e5f06c93704e
jjjjustine <catingan.janejustine@gmail.com>
2026-04-21 21:09:32 +0800

    added changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,25 +16,6 @@ const Home: React.FC = () => {
 
   
   return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                  <IonButtons  slot='start'>
-                    <IonMenuButton></IonMenuButton>
-                    </IonButtons>
-                            <IonTitle>Home</IonTitle>
-                            </IonToolbar>
-              </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-    
     <IonReactRouter>
       <IonTabs>
         <IonTabBar slot="bottom">
@@ -62,9 +43,7 @@ const Home: React.FC = () => {
          </IonRouterOutlet>
     </IonTabs>
     </IonReactRouter>
-
   );
 };
-
 
 export default Home;
