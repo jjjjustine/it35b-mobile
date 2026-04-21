a221e4412b26858b9482ad2fdca3498c5baa15db
jjjjustine <catingan.janejustine@gmail.com>
2026-04-21 09:08:10 +0800

    added hooks and serach bar

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,23 +1,91 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar  } from "@ionic/react";
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonList, IonMenuButton, IonPage, IonRow, IonSearchbar, IonTitle, IonToolbar  } from "@ionic/react";
+import { thumbsDownOutline, chatboxOutline, shareOutline } from 'ionicons/icons';
+import { useEffect, useState } from "react";
 
-const Search: React.FC = () => {
+
+const Feed: React.FC = () => {
+    const games = [
+        {name: 'Pokemon Yellow'},
+        {name: 'Megaman X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+        {name: 'Super Mario World'}
+      ];
+    
+      const[searchText, setSearchText] = useState("");
+      const[filtered, setFilteredGames] = useState(games);
+
+      useEffect(() => {
+        const debounce = setTimeout(() => {
+            const filtered = games.filter(game =>
+                game.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredGames(filtered);
+        },400);
+        return () => clearTimeout(debounce);
+    }, [searchText]);
+
     return(
         
         <IonPage>
-    <IonHeader>
+     <IonHeader>
             <IonToolbar>
                 <IonButtons slot="start">
                     <IonMenuButton></IonMenuButton>
-                                <IonTitle>Template</IonTitle>
                                 </IonButtons>
+                                <IonTitle>Feed</IonTitle>
                         </IonToolbar>
                     </IonHeader>
         
-        <IonContent fullscreen>
-                <h1>Search</h1>
-                </IonContent>
+        <IonContent className="ios-padding">
+            <IonSearchbar placeholder="Search games" 
+            value={searchText}
+            debounce={0}
+            onIonInput={(e)=> setSearchText(e.detail.value!)}>
+            </IonSearchbar>
+           
+            <IonList>
+                {filtered.map((item,index) => (
+                <IonCard key={index}>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>{item.name}</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+
+      <IonGrid>
+            <IonRow>
+                <IonCol>
+                    <IonButton fill="clear" expand="full">
+                    <IonIcon icon={thumbsDownOutline}></IonIcon>
+                    <IonLabel style={{marginlEft: '5px'}}>Like</IonLabel>
+                </IonButton>
+                </IonCol>
+
+                 <IonCol>
+                <IonButton fill="clear" expand="full">
+                    <IonIcon icon={chatboxOutline}></IonIcon>
+                    <IonLabel style={{marginleft: '5px'}}>Comment</IonLabel>
+                </IonButton>
+                </IonCol>
+
+                    <IonCol>
+                 <IonButton fill="clear" expand="full">
+                    <IonIcon icon={shareOutline}></IonIcon>
+                    <IonLabel style={{marginleft: '5px'}}>Share</IonLabel>
+                </IonButton>
+                </IonCol>
+              
+                </IonRow>
+            </IonGrid>
+            </IonCard>
+                ))}
+            </IonList>
+            </IonContent>
         </IonPage>
     )
 }
 
-export default Search;
+export default Feed;
