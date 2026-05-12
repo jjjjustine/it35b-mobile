56263a17918a6917dd9eda7a6720e1f84d47a06d
jjjjustine <catingan.janejustine@gmail.com>
2026-05-12 08:30:55 +0800

    added 0auth and also a supabase

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,19 @@
-import { IonButton, IonTitle, IonContent, IonMenuButton, IonHeader, IonPage, IonToolbar, useIonRouter  } from "@ionic/react";
+import { IonButton, IonTitle, IonContent, IonMenuButton, IonHeader, IonPage, IonToolbar, useIonRouter, IonIcon  } from "@ionic/react";
+import { logoGoogle } from "ionicons/icons";
+import { supabase } from "../lib/supabaeClient";
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
     const doLogin = () => {
         navigation.push('/app','forward','replace');
+    }
+    const signInWithGoogle = async() => {
+        await supabase.auth.signInWithOAuth({
+            provider: 'google',
+            options: {
+                redirectTo: `${window.location.origin}/app`
+            }
+        });
     }
     return (
         <IonPage>
@@ -14,7 +24,8 @@ const Login: React.FC = () => {
                         </IonToolbar>
                 </IonHeader>
                             <IonContent fullscreen className="ion-padding">
-                        <IonButton expand="full" onClick={doLogin}>
+                        <IonButton expand="full" onClick={signInWithGoogle} fill="outline">
+                            <IonIcon icon={logoGoogle} />
                                     Login
                      </IonButton>
                     </IonContent>
